329a09dbc6aa14058f20f71a4d95fa199fe91a2b
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-12-13 17:50:33 -0800

    update

--- test_trustee.py
+++ test_trustee.py
@@ -1,28 +1,55 @@
 from trustee import ClassificationTrustee, RegressionTrustee
-from pkl_cols import final_filtered_feature_cols as feature_cols
+# from pkl_cols import final_filtered_feature_cols as feature_cols
+from features import FINAL_MODEL
 from sklearn import tree, metrics
 from sklearn.metrics import classification_report, r2_score
 import pandas as pd
 import pickle
 import graphviz
 
-def main():
-    print("Importing validation data...")
-    df_train = pd.read_pickle('sample_traffic_time_10.pkl')
-    df_test = pd.read_pickle('validation_traffic_time_10.pkl')
+# most of this code is copied from the Trustee documentation
 
-    features_train = df_train[feature_cols]
+def main():
+    print("Importing data...")
+    df_train = pd.read_pickle('testing_2.pkl')
+    df_test = pd.read_pickle('validation_2.pkl')
+
+    print("Importing resolution model...")
+    with open('resolution_rf_all_FINAL_MODEL_FUZZY_time10_model.pkl', 'rb') as file:
+        res_model = pickle.load(file)
+
+    print("Importing startup model...")
+    with open('startup_time_rfr_all_FINAL_MODEL_FUZZY_time10_model.pkl', 'rb') as file:
+        start_model = pickle.load(file)
+
+    features_train = df_train[FINAL_MODEL]
+    features_test = df_test[FINAL_MODEL]
+
     expected_train_res = df_train['resolution']
-    features_test = df_test[feature_cols]
     expected_test_res = df_test['resolution']
 
-    with open('rf_resolution.pkl', 'rb') as file:
-        res_model = pickle.load(file)
-    trustee_res(features_train, expected_train_res, features_test, expected_test_res, res_model, 'rf_res')
+    expected_train_start = df_train['startup_time']
+    expected_test_start = df_test['startup_time']
 
-    with open('rf_startup_time.pkl', 'rb') as file:
-        start_model = pickle.load(file)
-    trustee_start(features_train, expected_train_res, features_test, expected_test_res, start_model, 'rf_start')
+    print("Resolution on training data:")
+    y_pred = res_model.predict(features_train)
+    print(metrics.classification_report(expected_train_res, y_pred, zero_division=0))
+
+    print("Resolution on test data:")
+    y_pred = res_model.predict(features_test)
+    print(metrics.classification_report(expected_test_res, y_pred, zero_division=0))
+
+    print("Startup on training data:")
+    y_pred = start_model.predict(features_train)
+    print(r2_score(expected_train_start, y_pred))
+
+    print("Startup on test data:")
+    y_pred = start_model.predict(features_test)
+    print(r2_score(expected_test_start, y_pred))
+
+    print("starting trustee")
+    trustee_start(features_train, expected_train_start, features_test, expected_test_start, start_model, 'rf_start')
+    trustee_res(features_train, expected_train_res, features_test, expected_test_res, res_model, 'rf_res')
 
 def trustee_res(X_train, y_train, X_test, y_test, model, filename):
     expected_classes = ['240', '360', '480', '720', '1080']
@@ -33,7 +60,7 @@ def trustee_res(X_train, y_train, X_test, y_test, model, filename):
 
     # train trustee
     trustee = ClassificationTrustee(expert=model)
-    trustee.fit(X_train, y_train, num_iter=50, num_stability_iter=10, samples_size=0.3, verbose=True)
+    trustee.fit(X_train, y_train, num_iter=50, num_stability_iter=10, samples_size=0.2, verbose=True)
     dt, pruned_dt, agreement, reward = trustee.explain()
     print(f"Model explanation training (agreement, fidelity): ({agreement}, {reward})")
     print(f"Model Explanation size: {dt.tree_.node_count}")
@@ -58,13 +85,12 @@ def trustee_res(X_train, y_train, X_test, y_test, model, filename):
 
 def trustee_start(X_train, y_train, X_test, y_test, model, filename):
     # Evaluate model accuracy
-    print("Model R2-score:")
     y_pred = model.predict(X_test)
-    print(r2_score(y_test, y_pred))
+    print("Model R2-score:", r2_score(y_test, y_pred))
 
     # Initialize Trustee and fit for classification models
     trustee = RegressionTrustee(expert=model)
-    trustee.fit(X_train, y_train, num_iter=50, num_stability_iter=10, samples_size=0.3, verbose=True)
+    trustee.fit(X_train, y_train, num_iter=50, num_stability_iter=3, samples_size=0.2, verbose=True)
 
     # Get the best explanation from Trustee
     dt, pruned_dt, agreement, reward = trustee.explain()
@@ -78,19 +104,21 @@ def trustee_start(X_train, y_train, X_test, y_test, model, filename):
 
     # Evaluate accuracy and fidelity of explanations
     print("Model explanation global fidelity:")
-    print(r2_score(y_pred, dt_y_pred))
+    print("Model R2-score:",r2_score(y_pred, dt_y_pred))
+
     print("Top-k Model explanation global fidelity:")
-    print(r2_score(y_pred, pruned_dt_y_pred))
+    print("Model R2-score:",r2_score(y_pred, pruned_dt_y_pred))
 
     print("Model explanation R2-score:")
-    print(r2_score(y_test, dt_y_pred))
+    print("Model R2-score:",r2_score(y_test, dt_y_pred))
+
     print("Top-k Model explanation R2-score:")
-    print(r2_score(y_test, pruned_dt_y_pred))
+    print("Model R2-score:",r2_score(y_test, pruned_dt_y_pred))
 
     export_dt(dt, pruned_dt, None, list(X_train.columns), filename)
 
 def export_dt(dt, pruned_dt, class_names, feature_names, filename):
-    # # Output decision tree to pdf
+    # # Output decision tree to pdf (can take a while to render)
     # dot_data = tree.export_graphviz(
     #     dt,
     #     class_names=class_names,
