c73d162040044fbbefbc5f6f079e958945112f4b
Kevin Nguyen <kevinngguyen1@gmail.com>
2024-12-14 00:02:24 -0800

    rename

--- split_data.py
+++ split_data.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 df = pd.read_pickle("all_traffic_time_10.pkl")
 
-df_train = df[df['service'] == 'youtube']
-df_testing = df[df['service'] != 'youtube']
+df_train = df[df['service'] == 'netflix']
+df_testing = df[df['service'] != 'netflix']
 
-df_train.to_pickle("youtube_train.pkl")
-df_testing.to_pickle("youtube_test.pkl")
+df_train.to_pickle("train.pkl")
+df_testing.to_pickle("test.pkl")
